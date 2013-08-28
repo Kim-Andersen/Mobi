2a819727238d4b1f3d2604342650af40d816612d
Kim-Andersen <kimandersen1@gmail.com>
2013-08-28 13:23:41 -0700

    Add header

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('Mobi', []);
 
 app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when("/employees", {controller: "EmployeeListCtrl", templateUrl: "partials/employee-list.html"});
 	$routeProvider.when("/employees/:id", {controller: "EmployeeDetailCtrl", templateUrl: "partials/employee-detail.html"});
-	$routeProvider.otherwise({controller: "EmployeeListCtrl", templateUrl: "partials/employee-list.html"});
 }]);
